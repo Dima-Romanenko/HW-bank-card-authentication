f276da86d6ff806e3a2dfacd524ba4a23f2b4b87
x3Dima <x3dima.romanenko@gmail.com>
2020-10-30 14:31:01 +0200

    added output and styles

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const outputBox = document.querySelector(".outputBox");
 let value = prompt("Введите номер карты");
 
+// Функция удаляет из номера лишние символы и пробелы, проверяет количество символов
+//  и возвращает массив со значениями выстроенными в обратном порядке
 let processData = (data) => {
   let stringToArray = data.split(" ").join("");
   let dataArray = [...stringToArray].filter((el) => {
@@ -10,7 +13,7 @@ let processData = (data) => {
 
   if (dataArray.length === 16) return dataArray.reverse();
 };
-
+// Алгоритм луна для проверки коректности номера
 let checkNumberWithLune = (data) => {
   let sum = 0;
   data = convertToNumber(data);
@@ -29,12 +32,12 @@ let checkNumberWithLune = (data) => {
     return false;
   }
 };
-
+// Функция приводит элементы массива к числу
 let convertToNumber = (data) => {
   data = data.map((el) => +el);
   return data;
 };
-
+// Функция приводит элементы массива к строке и возвращает строку
 let convertArrayToString = (arr) => {
   let str = "";
   for (el of arr) {
@@ -42,7 +45,7 @@ let convertArrayToString = (arr) => {
   }
   return str;
 };
-
+// Функция проверяет к какой платежной системе принадлежит номер карты
 let compareCardInn = (code) => {
   let innWithTwoNum = "";
   let innWithFourNum = "";
@@ -78,33 +81,54 @@ let compareCardInn = (code) => {
     return undefined;
   }
 };
-
-let mainFunc = (data) => {
-  let cardNumber = processData(data);
-  let check = checkNumberWithLune(cardNumber);
-  let paymentSystem = compareCardInn(cardNumber);
-  if (check && paymentSystem === undefined) {
+// Функция проверяет данные и формирует обьект ответа
+let createObject = (number, check, payment) => {
+  if (check && payment === undefined) {
     return {
-      number: convertArrayToString(cardNumber),
+      card: convertArrayToString(number),
       correct: check,
-      paymentSystem: "",
+      payment: "",
       accepted: false,
     };
-  } else if (check && paymentSystem !== undefined) {
+  } else if (check && payment !== undefined) {
     return {
-      number: convertArrayToString(cardNumber),
+      card: convertArrayToString(number),
       correct: check,
-      paymentSystem,
+      payment,
       accepted: true,
     };
   } else {
     return {
-      number: convertArrayToString(cardNumber),
+      card: convertArrayToString(number),
       correct: check,
-      paymentSystem,
+      payment,
       accepted: false,
     };
   }
 };
 
-console.log(mainFunc(value));
+// Функция вывода данных
+
+let outputFunc = (obj, place) => {
+  place.style.display = "flex";
+  place.innerHTML = `
+  <span>Card number: ${obj.card}</span>
+  <span>Correct: ${obj.correct}</span>
+  <span>Payment system: ${obj.payment}</span>
+  <span>Accepted: ${obj.accepted}</span>
+
+  `;
+};
+
+// Главная функция, по порядку запускает функции
+let mainFunc = (data) => {
+  let cardNumber = processData(data);
+  let check = checkNumberWithLune(cardNumber);
+  let paymentSystem = compareCardInn(cardNumber);
+  let object = createObject(cardNumber, check, paymentSystem);
+  outputFunc(object, outputBox);
+};
+
+if (value !== null || value !== "") {
+  mainFunc(value);
+}
